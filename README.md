4e51df8996df215973dcddf8a93fb89785bf1702
Rodolphe <dutchr@live.fr>
2021-11-05 10:48:14 +0100

    :sparkles: Adding Methods Create/Update/Delete

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ To create a admin subfolder (Linux distrib)
 ```bash
 symfony console make:controller admin\\AdminAlimentController
 ```
+
+To delete an aliment 
+
+```bash
+// framework.yaml
+http_method_override: true
+```
+
--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aliment;
+use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,4 +22,34 @@ class AdminAlimentController extends AbstractController
             'aliments' => $aliments,
         ]);
     }
+    #[Route('/admin/aliments/create', name:'admin_aliments_create')]
+    #[Route('/admin/aliments/{id}', name:"admin_aliments_edit", methods:['GET'|'POST'])]
+      public function createAndEditAction(Aliment $aliment = null, Request $request, EntityManagerInterface $manager) 
+      {
+        if(!$aliment){
+          $aliment = new Aliment();
+        }
+        $form = $this->createForm(AlimentType::class, $aliment);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+          $manager->persist($aliment);
+          $manager->flush();
+          return $this->redirectToRoute('admin_aliments');
+        }
+
+        return $this->render('admin/admin_aliment/edit_aliment.html.twig', [
+          'aliment' => $aliment,
+          'form' => $form->createView(),
+          'modification' => $aliment->getId() !== null
+        ]);
+      }
+      #[Route("/admin/aliments/{id}", name:"admin_aliments_suppression", methods:['delete'])]
+        public function suppression(Aliment $aliment, Request $request, EntityManagerInterface $manager) {
+          if($this->isCsrfTokenValid('SUP' . $aliment->getId(), $request->get('_token'))) {
+              $manager->remove($aliment);
+              $manager->flush();
+              return $this->redirectToRoute('admin_aliments');
+          }
+      }
+
 }
