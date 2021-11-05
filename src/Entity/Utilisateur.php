cbad0c8a45630a01d6518a518a2fdcaf3239e515
Rodolphe <dutchr@live.fr>
2021-11-05 14:32:36 +0100

    :sparkles: Adding User + Inscription Form

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+    private $password_confirmation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+    public function getPasswordConfirmation(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPasswordConfirmation(string $passwordConfirmation): self
+    {
+        $this->passwordConfirmation = $passwordConfirmation;
+
+        return $this;
+    }
+}
